a1d6ff766a53886120a39ce64eb06c8233292c7a
reiyuchan <85351652+reiyuchan@users.noreply.github.com>
2022-01-04 13:06:00 +0200

    added animation to navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
-$("#"+"list-div").slideUp(10).slideDown(1000);
-$('#' + "heading2-div").slideUp(10).slideDown(1000)
+$('#'+'list-div').slideUp(10).slideDown(1000);
+$('#'+'heading2-div').slideUp(10).slideDown(1000)
 $('h2').fadeOut("slow").fadeIn("slow");
+$('#' + 'nav-bar').slideUp(10).slideDown(1000)
