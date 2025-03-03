27a215ed7d91a66883052f9a805a271cda9b0e48
reiyuchan <iwakura.rei.tk@gmail.com>
2025-03-03 06:56:33 +0200

    add not found view to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ReposView from '../views/ReposView.vue'
+import NotFoundView from '../views/_NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/:catchAll(.*)',
+      name: 'notfound',
+      component: NotFoundView
+    },
     {
       path: '/',
       name: 'home',
